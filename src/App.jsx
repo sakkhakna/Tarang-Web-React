05fc03938a08cd261613b8a897e8857d08e7c369
Sak Khakna <sthissakkhakna@gmail.com>
2024-03-06 15:28:17 +0700

    registerTournament

--- src/App.jsx
+++ src/App.jsx
@@ -16,37 +16,42 @@ import HostingForm from "./pages/user/HostingForm";
 import Badminton from "./pages/user/sport/Badminton";
 import ChallengeForm from "./pages/user/ChallengeForm";
 import JoinForm from "./pages/user/JoinForm";
+import RegisterTournament from "./pages/user/RegisterTournament";
 
 function App() {
-  const role = "user";
-  return (
-    <Router>
-      <Routes>
-        {role === "admin" ? (
-          <Route path="/" element={<AdminLayout />}>
-            <Route path="/" element={<Dashboard />} />
-            <Route path="/venue" element={<AdminVenue />} />
-            <Route path="/team" element={<AdminTeam />} />
-            <Route path="/reserve" element={<AdminReservation />} />
-            <Route path="/user" element={<User />} />
-          </Route>
-        ) : (
-          <Route path="/" element={<UserLayout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/team" element={<Team />} />
-            <Route path="/reservation" element={<ReservationForm />} />
-            <Route path="/profile" element={<Profile />} />
-            <Route path="/hosting" element={<HostingForm />} />
-            <Route path="/sport" element={<Badminton />} />
-            <Route path="/challenge" element={<ChallengeForm />} />
-            <Route path="/join" element={<JoinForm />} />
-          </Route>
-        )}
-        <Route path="/signin" element={<SignIn />} />
-        <Route path="/signup" element={<SignUp />} />
-      </Routes>
-    </Router>
-  );
+	const role = "user";
+	return (
+		<Router>
+			<Routes>
+				{role === "admin" ? (
+					<Route path="/" element={<AdminLayout />}>
+						<Route path="/" element={<Dashboard />} />
+						<Route path="/venue" element={<AdminVenue />} />
+						<Route path="/team" element={<AdminTeam />} />
+						<Route path="/reserve" element={<AdminReservation />} />
+						<Route path="/user" element={<User />} />
+					</Route>
+				) : (
+					<Route path="/" element={<UserLayout />}>
+						<Route path="/" element={<Home />} />
+						<Route path="/team" element={<Team />} />
+						<Route path="/reservation" element={<ReservationForm />} />
+						<Route path="/profile" element={<Profile />} />
+						<Route path="/hosting" element={<HostingForm />} />
+						<Route path="/sport" element={<Badminton />} />
+						<Route path="/challenge" element={<ChallengeForm />} />
+						<Route path="/join" element={<JoinForm />} />
+						<Route
+							path="/registertournament"
+							element={<RegisterTournament />}
+						/>
+					</Route>
+				)}
+				<Route path="/signin" element={<SignIn />} />
+				<Route path="/signup" element={<SignUp />} />
+			</Routes>
+		</Router>
+	);
 }
 
 export default App;
