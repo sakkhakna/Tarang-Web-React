474b297f247f0605d63d17c238686fdb2518a77f
Sak Khakna <sthissakkhakna@gmail.com>
2024-03-13 14:41:19 +0700

    Register Tournament

--- src/ui/shared/Input.jsx
+++ src/ui/shared/Input.jsx
@@ -3,59 +3,58 @@ import { FaRegEye } from "react-icons/fa6";
 import { FaRegEyeSlash } from "react-icons/fa6";
 
 function Input({
-  title,
-  type,
-  placeholder,
-  id,
-  onChange,
-  value,
-  isRequired,
-  disabled,
-  customClass,
+	title,
+	type,
+	placeholder,
+	id,
+	onChange,
+	value,
+	isRequired,
+	disabled,
+	customClass,
 }) {
-
-  const defaultClass =
+	const defaultClass =
 		"rounded-xl w-full px-4 py-2 border focus:outline-none focus:ring focus:border-[#2AD5A5]";
 	const InputClass = customClass
 		? `${defaultClass} ${customClass}`
 		: defaultClass;
 
-  const isPasswordInput = type === "password";
-  const [showPassword, setShowPassword] = useState(false);
+	const isPasswordInput = type === "password";
+	const [showPassword, setShowPassword] = useState(false);
 
-  const togglePasswordVisibility = () => {
-    setShowPassword(!showPassword);
-  };
-  return (
-    <div className="flex flex-col gap-2">
-      <label htmlFor={id} className="text-sm md:text-base">
-        {title}
-      </label>
-      <div className="relative flex">
-        <input
-          type={isPasswordInput && showPassword ? "text" : type}
-          placeholder={
-            isPasswordInput && showPassword ? "password" : placeholder
-          }
-          id={id}
-          onChange={onChange}
-          value={value}
-          required={isRequired}
-          className={InputClass}
-          disabled={disabled}
-        />
-        {isPasswordInput && (
-          <button
-            type="button"
-            onClick={togglePasswordVisibility}
-            className="svg-size absolute top-[12px] right-3"
-          >
-            {showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
-          </button>
-        )}
-      </div>
-    </div>
-  );
+	const togglePasswordVisibility = () => {
+		setShowPassword(!showPassword);
+	};
+	return (
+		<div className="flex flex-col gap-2 w-full">
+			<label htmlFor={id} className="text-sm md:text-base">
+				{title}
+			</label>
+			<div className="relative flex">
+				<input
+					type={isPasswordInput && showPassword ? "text" : type}
+					placeholder={
+						isPasswordInput && showPassword ? "password" : placeholder
+					}
+					id={id}
+					onChange={onChange}
+					value={value}
+					required={isRequired}
+					className={InputClass}
+					disabled={disabled}
+				/>
+				{isPasswordInput && (
+					<button
+						type="button"
+						onClick={togglePasswordVisibility}
+						className="svg-size absolute top-[12px] right-3"
+					>
+						{showPassword ? <FaRegEyeSlash /> : <FaRegEye />}
+					</button>
+				)}
+			</div>
+		</div>
+	);
 }
 
 export default Input;
