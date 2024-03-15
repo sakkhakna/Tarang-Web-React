60b66054640d6bd834722c2d58efb93856ddd972
Sak Khakna <sthissakkhakna@gmail.com>
2024-03-15 14:05:51 +0700

    fix gap and borde inr single venue page

--- src/pages/user/venue/SingleVenue.jsx
+++ src/pages/user/venue/SingleVenue.jsx
@@ -8,14 +8,14 @@ import { FaTelegramPlane } from "react-icons/fa";
 function SingleVenuePage() {
 	return (
 		<div>
-			<div className="bg-[#f5f5f5] max-w-7xl flex gap-4 p-10 xl:px-0 mt-[98px]">
-				<div className="flex flex-col gap-10 w-[70%]">
-					<div className="flex flex-col gap-4 bg-white p-10">
+			<div className="bg-[#f5f5f5] max-w-7xl flex gap-6 p-10 xl:px-0">
+				<div className="flex flex-col gap-6 w-[70%]">
+					<div className="flex flex-col gap-4 bg-white p-10 rounded-lg shadow">
 						<h1 className="text-gray-300">type of venue</h1>
 						<h1 className="text-2xl md:text-4xl font-bold">
 							Name of the Venue
 						</h1>
-						<div className="bg-black w-full p-2">
+						<div className="bg-gray-800 w-full p-2">
 							<h1 className="text-2xl text-white">Option</h1>
 						</div>
 						<div className="grid grid-cols-3">
@@ -34,7 +34,7 @@ function SingleVenuePage() {
 						</div>
 					</div>
 
-					<div className="flex flex-col bg-[#ffffff] justify-center p-10 gap-10">
+					<div className="flex flex-col bg-[#ffffff] justify-center p-10 gap-4 rounded-lg shadow">
 						<h1 className="font-bold text-xl">Description and details</h1>
 						<div className="flex flex-col gap-4 text-2xl">
 							<div className="flex gap-4 justify-center">
@@ -51,7 +51,7 @@ function SingleVenuePage() {
 						</div>
 					</div>
 
-					<div className="flex flex-col gap-10 bg-white p-10">
+					<div className="flex flex-col gap-4 bg-white p-10 rounded-lg shadow">
 						<h1 className="font-bold text-xl">Amenities</h1>
 						<div className="flex gap-10">
 							<div className="flex gap-2">
@@ -78,8 +78,8 @@ function SingleVenuePage() {
 					</div>
 				</div>
 				<div className="flex flex-col gap-4 w-[30%]">
-					<Button customClass="w-full">Book</Button>
-					<div className="w-full p-10 flex flex-col gap-10 bg-white">
+					<Button customClass="md:w-full text-white">Book</Button>
+					<div className="w-full p-10 flex flex-col gap-4 bg-white rounded-lg shadow">
 						<h1 className="text-xl font-bold">Contact Manager</h1>
 						<div className="w-full">
 							<div className="flex flex-col gap-4">
@@ -112,7 +112,7 @@ function SingleVenuePage() {
 										placeholder="Enter Your Message"
 									></textarea>
 								</div>
-								<Button customClass="w-full flex justify-center gap-4">
+								<Button customClass="md:w-full flex justify-center gap-3 text-white items-center">
 									<FaTelegramPlane className="w-[20px] h-[20px]" />
 									<h1>Sent Message</h1>
 								</Button>
