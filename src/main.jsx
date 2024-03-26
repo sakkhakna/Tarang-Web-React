39287d06e208c061d83cc52701bc239166ff3385
Sodara Sou <sodarasou168@gmail.com>
2024-03-26 08:49:21 +0700

    Refactor CSS and JSX formatting

--- src/main.jsx
+++ src/main.jsx
@@ -7,11 +7,11 @@ import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 const queryClient = new QueryClient();
 ReactDOM.createRoot(document.getElementById("root")).render(
-	<React.StrictMode>
-		<AppProvider>
-			<QueryClientProvider client={queryClient}>
-				<App />
-			</QueryClientProvider>
-		</AppProvider>
-	</React.StrictMode>
+  <React.StrictMode>
+    <AppProvider>
+      <QueryClientProvider client={queryClient}>
+        <App />
+      </QueryClientProvider>
+    </AppProvider>
+  </React.StrictMode>
 );
