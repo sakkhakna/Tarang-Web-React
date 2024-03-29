a8668b15640ddd3bf83096c4eb4776c3e3ba601d
Sodara Sou <sodarasou168@gmail.com>
2024-03-29 11:12:32 +0700

    Refactor ProtectedRoute component

--- src/pages/auth/ProtectedRoute.jsx
+++ src/pages/auth/ProtectedRoute.jsx
@@ -3,12 +3,15 @@ import { useQuery } from "@tanstack/react-query";
 import { checkUserStatusLogin } from "@/service/user.api";
 import Spinner from "@/ui/Spinner";
 import Profile from "../user/Profile";
+import { useState } from "react";
 
 function ProtectedRoute() {
-  const { data, isLoading } = useQuery({
-    queryKey: ["checkUserStatusLogins"],
-    queryFn: checkUserStatusLogin,
-  });
+  // const { data, isLoading } = useQuery({
+  //   queryKey: ["checkUserStatusLogins"],
+  //   queryFn: checkUserStatusLogin,
+  // });
+  const [message, setMessage] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
   if (isLoading) {
     return <Spinner />;
   }
