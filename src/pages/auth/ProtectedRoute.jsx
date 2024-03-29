35dca04f85da4802352898ae19c788ce15485468
Sodara Sou <sodarasou168@gmail.com>
2024-03-29 10:19:20 +0700

    Refactor ProtectedRoute component to display a spinner while loading and navigate to signin page if unauthorized

--- src/pages/auth/ProtectedRoute.jsx
+++ src/pages/auth/ProtectedRoute.jsx
@@ -1,14 +1,21 @@
 import { Outlet, Navigate } from "react-router-dom";
 import { useQuery } from "@tanstack/react-query";
 import { checkUserStatusLogin } from "@/service/user.api";
+import Spinner from "@/ui/Spinner";
 
 function ProtectedRoute() {
-  const { data } = useQuery({
+  const { data, isLoading } = useQuery({
     queryKey: ["checkUserStatusLogins"],
     queryFn: checkUserStatusLogin,
   });
-  console.log(data);
-  return data === 401 ? <Navigate to="/signin" /> : <Outlet />;
+  if (isLoading) {
+    return <Spinner />;
+  }
+  return data.message === "Unauthorized" ? (
+    <Navigate to="/signin" />
+  ) : (
+    <Outlet />
+  );
 }
 
 export default ProtectedRoute;
